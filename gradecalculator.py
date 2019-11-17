b6c4e5f876289da5c12ab3f1d315bbb0d237f96b
ykisana <=>
2019-11-17 12:37:29 +0000

    working on prompts

--- gradecalculator.py
+++ gradecalculator.py
@@ -11,12 +11,17 @@ def main():
         grade = float(input("Enter value of grade in percent: "))
         calculated = grade*weight
         totalCalculated = totalCalculated + calculated
-        print(totalCalculated/totalWeight)
+        print("%.2f" % totalCalculated/totalWeight)
 
         endProgram = input("End Program Y/N: ")
         if endProgram == "Y":
             running = False
 
+def prompts(x):
+    print("Select Funtion")
+    print("1. Calculate New Grade")
+    print("2. Continue Calculating Grade")
+    print("3. Calculate Grade Needed to Pass")
 
 
 main()
